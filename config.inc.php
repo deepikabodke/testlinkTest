36cba3a46ab03686fc4a7d4d02267e30f878c458
Deepika <deepika.bodke@kissht.com>
2024-03-18 16:52:04 +0530

    changed log path

--- config.inc.php
+++ config.inc.php
@@ -308,7 +308,7 @@ $tlCfg->smarty_debug = false;
  *  for security reasons (see http://itsecuritysolutions.org/2012-08-13-TestLink-1.9.3-multiple-vulnerabilities/)
  *  put it out of reach via web or configure access denied.
  */
-$tlCfg->log_path = '/var/testlink/logs/'; /* unix example */
+$tlCfg->log_path = '/var/www/html/logs/'; /* unix example */
 if (($lp = getenv('TESTLINK_LOG_PATH'))) {
   $tlCfg->log_path = trim($lp);
 }
